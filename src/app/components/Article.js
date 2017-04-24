41e7241816507d06b4519fcd502cbd7f80d35b2c
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-24 16:08:22 -0600

    Hardcoded blog[0] and working

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -1,3 +1,25 @@
+// "use strict";
+//
+// import React from 'react';
+//
+// import styles from '../styles/App.css';
+// import blogData from './allblogdata.json';
+//
+// export default class Article extends React.Component {
+//   render () {
+//     console.log(blogData);
+//     return (
+//       <div className="individualBlog">
+//         <h2>{blogData[2].title}</h2>
+//         <h3>{blogData[2].article[0]}</h3>
+//         <h3>{blogData[2].tags}</h3>
+//
+//       </div>
+//     )
+//   }
+// }
+
+
 "use strict";
 
 import React from 'react';
@@ -6,15 +28,24 @@ import styles from '../styles/App.css';
 import blogData from './allblogdata.json';
 
 export default class Article extends React.Component {
+
   render () {
-    console.log(blogData);
+    let paras = blogData[0].article;
+    // console.log('paras: ', paras)
+
+    let parasformatted = paras.map((para, i, a) => {
+      return (para);
+    })
+console.log(parasformatted);
+
     return (
       <div className="individualBlog">
-        <h2>{blogData[2].title}</h2>
-        <h3>{blogData[2].article}</h3>
-        <h3>{blogData[2].tags}</h3>
-
-
+        <h2>{blogData[0].title}</h2>
+        {/* <p>{ parasformatted }</p> */}
+        {paras.map((para, i, a) => {
+          return (<p>{para}</p>);
+        })}
+        <h3>{blogData[0].tags}</h3>
       </div>
     )
   }
