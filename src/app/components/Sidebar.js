e0edf58cb94dbcf6aaf22b4746b1a25a6e5f8c4f
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-28 16:23:16 -0600

    new Titles tile working

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -11,23 +11,7 @@ export default class Sidebar extends React.Component {
     const blogData = this.props.blogData;
     const tags = [];
     const dates = [];
-    const titles = [];
-// make tags, dates and title arrays
-//     function makeSidebarArrays (array, category) {
-//       blogData.map((individArticle, i, a)=>{
-//       // const individArray = individArticle.tags
-//       const individArray = category;
-//
-//       console.log(individArray);
-//       individArray.map((c, i, a) => {
-//         if(!array.includes(c)) {
-//           array.push(c)
-//         }
-//       })
-// })}
-// const test = makeSidebarArrays(tags, blogData[i].tags);
-//
-// console.log(test);
+    const title = [];
 
     blogData.map((individArticle, i, a)=>{
       const tagsArray = individArticle.tags
@@ -37,24 +21,44 @@ export default class Sidebar extends React.Component {
         }
       })
     })
-    //alphabetize the categoryArrays:
-    tags.sort(function (a,b) {
-      var nameA = a.toUpperCase(); // ignore upper and lowercase
-      var nameB = b.toUpperCase(); // ignore upper and lowercase
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-    });
+
+    blogData.map((individArticle, i)=>{
+       title.push(individArticle.title)
+    })
+
+    function alphabetize(array) {
+      array.sort(function (a,b) {
+        var nameA = a.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      });
+    }
+
+    alphabetize(tags)
+    alphabetize(title)
+    // //alphabetize the categoryArrays:
+    // tags.sort(function (a,b) {
+    //   var nameA = a.toUpperCase(); // ignore upper and lowercase
+    //   var nameB = b.toUpperCase(); // ignore upper and lowercase
+    //   if (nameA < nameB) {
+    //     return -1;
+    //   }
+    //   if (nameA > nameB) {
+    //     return 1;
+    //   }
+    // });
 
     return (
       <div className="sidebar">
         <p>TILES (titles, filters, dates)</p>
       {/*this Tile is for tags,... but there will be more */}
         <Tile categoryArray = {tags} />
-
+        <Tile categoryArray = {title} />
       </div>
     )
   }
