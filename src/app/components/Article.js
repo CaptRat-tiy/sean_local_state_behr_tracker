a1e0161c0ebf4b01f4cc6eb15176e13a4b7fe067
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-30 21:15:41 -0600

    Remove tiny square 'img' at bottom of each article

--- src/app/components/Article.js
+++ src/app/components/Article.js
@@ -34,7 +34,6 @@ export default class Article extends React.Component {
                     </p>
                    )}
                 )}
-                <img src={c.src} />
               </div>
             )}
           )}
