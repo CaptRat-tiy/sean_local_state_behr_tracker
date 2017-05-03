3eeb88e079b7ea6196d40f11d32154aa46dce694
captrat <jpatricklogan@gmail.comgit config user.name>
2017-05-03 16:23:41 -0600

    WIP firebase ain't talking to React.  Yet.

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -3,32 +3,60 @@
 import React from 'react';
 
 import styles from '../styles/App.css';
-import observedBehaviors from './observedBehaviors.json';
+import * as firebase from "firebase";
 
+var config = {
+    apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
+    authDomain: "behavioral-tracker-app.firebaseapp.com",
+    databaseURL: "https://behavioral-tracker-app.firebaseio.com",
+    projectId: "behavioral-tracker-app",
+    storageBucket: "behavioral-tracker-app.appspot.com",
+    messagingSenderId: "119916788968"
+  };
+
+firebase.initializeApp(config);
+
+const fbRef= firebase.database().ref();
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
+    this.changeIntoArray=this.changeIntoArray.bind(this);
     this.state = {
-      observedBehaviors: observedBehaviors,
+      courseData: {},
+
     }
   }
 
+  componentWillMount() {
+    fbRef.on("child_added", (snapshot) => {
+      this.setState({
+        courseData: snapshot.val(),
+        students: snapshot.val().studentArray,
+        teacherInfo: snapshot.val().teacherID,
+        behaviors: snapshot.val().behaviors,
+        testing: this.changeIntoArray(snapshot.val().studentArray)
+      })
+    }).bind(this)
+  }
+
+changeIntoArray(object){
+  console.log(object);
+  const array = []
+  object.map((c, i, a)=>{
+    array.push(c)
+  })
+  return array
+}
   render () {
-    // console.log(observedBehaviors);
-    // const observedBehaviors=this.props.observedBehaviors;
+    const teacherName = this.state.teacherInfo
+
+    const testing=this.state.testing
+    console.log(testing);
     return (
+
       <div>
-        {
-        observedBehaviors.map((c,i,a) =>{
-          return (
-            <div key={i}>
-              <p>{"Hellp World!"}</p>
-              <p>{console.log(c.TEACHER1.firstname)}</p>
-            </div>
-          )
-        })
-        }
+        <h1>{testing}'s Course</h1>
       </div>
     )
   }
