78dd7132ce159f0810a97cf2a4d0ce0c7a3d30e8
Patrick Logan <JPatrickLogan@gmail.com>
2017-05-03 07:23:19 -0600

    Include attendance and interval in Student object

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -4,19 +4,25 @@ import React from 'react';
 
 import styles from '../styles/App.css';
 import observedBehaviors from './observedBehaviors.json';
+console.log(observedBehaviors);
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      observedBehaviors: observedBehaviors,
+    }
+  }
 
   render () {
-    const observedBehaviors = this.props.observedBehaviors;
-
+    const observedBehaviors=this.props.observedBehaviors;
     return (
       <div>
         {
         observedBehaviors.map((c,i,a) =>{
           return (
             <div>
-              <h6>{observedBehaviors.TEACHER}</h6>
+              <h6>{observedBehaviors.ATTENDANCE.tardy}</h6>
             </div>
           )
         })
