cf33ec4197bd6c2ce7b256e403e7e00677412862
Patrick Logan <patricklogan@Patricks-iMac-2.local>
2017-05-04 10:55:38 -0600

    Displaying real fb data on frontend SUCCESS

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,34 +1,67 @@
 "use strict";
 
 import React from 'react';
+import _ from 'underscore';
+import * as firebase from 'firebase';
+
 
 import styles from '../styles/App.css';
 import observedBehaviors from './observedBehaviors.json';
 
+var config = {
+    apiKey: "AIzaSyAjB5xxpo_eOVJ7LFoDJUN51TGXyhkq1IQ",
+    authDomain: "behavioral-tracker-app.firebaseapp.com",
+    databaseURL: "https://behavioral-tracker-app.firebaseio.com",
+    projectId: "behavioral-tracker-app",
+    storageBucket: "behavioral-tracker-app.appspot.com",
+    messagingSenderId: "119916788968"
+  };
+
+firebase.initializeApp(config);
+
+const fbRef = firebase.database().ref()
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
+    this.changeIntoArray=this.changeIntoArray.bind(this)
+
     this.state = {
-      observedBehaviors: observedBehaviors,
+      courseData: {},
+      students: [],
+
     }
   }
 
+  componentWillMount(){
+    fbRef.on("child_added", (snapshot)=>{
+      const students = this.changeIntoArray(snapshot.val().studentArray)
+      this.setState({
+        students: students
+
+      })
+    })
+  }
+
+  changeIntoArray(object){
+    const newArray = []
+    _.map(object, function(c,i,a){
+      newArray.push(c)
+    })
+    return newArray
+  }
+
   render () {
-    // console.log(observedBehaviors);
-    // const observedBehaviors=this.props.observedBehaviors;
+    const students = this.state.students
+
+    console.log(this.state.students);
     return (
       <div>
-        {
-        observedBehaviors.map((c,i,a) =>{
-          return (
-            <div key={i}>
-              <p>{"Hellp World!"}</p>
-              <p>{console.log(c.TEACHER1.firstname)}</p>
-            </div>
-          )
-        })
+        {students.map((studentObject, index)=>{
+          return <p key={studentObject.lastName}>{studentObject.firstName} {studentObject.lastName}</p>
         }
+
+        )}
       </div>
     )
   }
