1dec93ddba64eb433a764a1fa7104b026814ff28
Patrick Logan <jpatricklogan@gmail.com>
2017-05-07 23:17:38 -0600

    Student.js not working

--- src/app/components/Student.js
+++ src/app/components/Student.js
@@ -7,9 +7,11 @@ import styles from '../styles/App.css';
 export default class Student extends React.Component{
   render(){
     return (
+
       <div>
-      <h1>Student Name</h1>
-         <h3 className="lastName">{this.props.student}</h3>
+
+        <h1>Carrie Name</h1>
+        <h3 className="lastName">{this.props.lastName}</h3>
       </div>
     )
   }
