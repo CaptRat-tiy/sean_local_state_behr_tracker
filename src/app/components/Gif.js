7107aef868dad1f865193fb202adcbde5428dc7c
Patrick Logan <jpatricklogan@gmail.com>
2017-05-16 08:24:26 -0600

    flexboxed Header

---
+++ src/app/components/Gif.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from 'react';
+
+import styles from '../App.css';
+
+export default class Gif extends React.Component{
+  render(){
+    return (
+        
+    )
+  }
+}
--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -9,11 +9,18 @@ import styles from '../App.css';
 export default class NavBar extends React.Component{
   render(){
     return (
-        <header>
-            <h1>Behavior Tracking App ('navbar component')</h1>
+      <div>
+        <header className="header">
+          <div className="gif">
+            <p></p>
+          </div>
+          <div className="headerText">
+          <h1>Behavior Tracking App ('navbar component')</h1>
             <Link to="/about"><p>About</p></Link>
             <Link to="/student"><p>Student</p></Link>
+          </div>
         </header>
+      </div>
     )
   }
 }
