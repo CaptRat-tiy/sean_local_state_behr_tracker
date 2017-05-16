96eef05ba53ed8f7c15f5dbb3d6736214bc7664f
captrat <jpatricklogan@gmail.comgit config user.name>
2017-05-16 08:59:43 -0600

    WIP: flexbox the Header

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -10,9 +10,14 @@ export default class NavBar extends React.Component{
   render(){
     return (
         <header>
+          <div className="gifImage">
+            <img src="./../image/headerGifs/a-a-ron.gif" />
+          </div>
+          <div className="content">
             <h1>Behavior Tracking App ('navbar component')</h1>
             <Link to="/about"><p>About</p></Link>
             <Link to="/student"><p>Student</p></Link>
+          </div>  
         </header>
     )
   }
