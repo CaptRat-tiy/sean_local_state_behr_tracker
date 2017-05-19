d83eb11604beae95fb46ff9b55ac26c63659a0a0
Patrick Logan <jpatricklogan@gmail.com>
2017-05-19 06:31:24 -0600

    Add remaining beh'rs to local state

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -43,6 +43,30 @@ export default class App extends React.Component {
               lastName: "Lilly",
               studentID: 1 ,
             },
+            2: {
+              firstName: "Sean",
+              image: "../image/student-icons/red_boy.png" ,
+              lastName: "Fidelis",
+              studentID: 2 ,
+            },
+            3: {
+              firstName: "Rob",
+              image: "../image/student-icons/dark_haired_boy.png",
+              lastName: "Johnson",
+              studentID: 3 ,
+            },
+            4: {
+              firstName: "Nancy",
+              image: "../image/student-icons/dark_haired_girl.png",
+              lastName: "Drew",
+              studentID: 4 ,
+            },
+            5: {
+              firstName: "Kamie",
+              image: "../image/student-icons/Kamie_look_alike.png",
+              lastName: "Lin",
+              studentID: 5 ,
+            },
           },
 
           teacherInfo: {
@@ -64,6 +88,26 @@ export default class App extends React.Component {
         crying:{
           image: "../image/frequent crying/frequent_crying_red.png",
           name: "Crying"
+        },
+        fighting:{
+          image: "../image/aggressive/aggressive_speech_red.png",
+          name: "Fighting"
+        },
+        hands_to_self :{
+          image: "../image/hands to self/hands_to_self_red.png",
+          name: "Hands to Self"
+        },
+        not_facing_front:{
+        image: "../image/distraction/not_facing_front_red.png",
+        name: "Not Facing Front"
+      },
+        self_distracting:{
+        image: "../image/distraction/self-distracting_red.png",
+        name: "Self Distracting"
+        },
+        withdrawn:{
+        image: "../image/withdrawn/withdrawn_red.png" ,
+        name: "Withdrawn"
         }
       },
     }
