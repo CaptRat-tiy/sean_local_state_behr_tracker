5512dda4a5ed825979d13bebed955124bc35bbc8
Patrick Logan <jpatricklogan@gmail.com>
2017-09-17 21:23:00 -0600

    WIP: time of day still not displaying correctly

--- src/app/components/IndividualBehaviorTracking.js
+++ src/app/components/IndividualBehaviorTracking.js
@@ -10,14 +10,14 @@ export default class IndividualBehaviorTracking extends React.Component{
 
 // (does this work for 8:05am, instead of 8:5am?)
     let minutes = instance.time.minutes
-    if (minutes = 0){
+    if (minutes == 0){
       minutes = "00"
     }
     else if (minutes < 10){
       minutes = "0" + minutes
     }
 
-//In Time of Day, minutes under 10 do not display 2 numbers.  So 8:05pm  is 20:0 !?
+//In Time of Day, minutes do not display!?
     return (
       <div className="busted">
         <div className="bustedBehavior">
